c9cfda7d9b306da4314e85cbda301ad3e6c1e1e5
Ellen-Vitoria <ellen.vitoria.ogs@gmail.com>
2022-08-23 21:39:30 -0300

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-// Import stylesheets
-import './style.css';
-
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>Hello World</h1>`;
+//Functional programming example
+function getNumber(inputID) {
+  const value = document.getElementById(inputID).value;
+  return parseInt(value);
+}
+//funcao recursiva - pilha de chamada(gasta mais memória)
+function getFunctionalFactorial(n) {
+  if (n == 1) return 1;
+  else return n * getFunctionalFactorial(n - 1);
+}
+function displaySum(value) {
+  document.getElementById('func-sum-result').textContent = value;
+}
+document.getElementById('func-btn').onclick = function handler() {
+  const res = getFunctionalFactorial(getNumber('input-n'));
+  displaySum(res);
+}
